02152bdfdd280a9c2e785d5c9d20d86119d2e20a
JackKingLeon <jackleon415@gmail.com>
2024-10-20 14:22:53 -0700

    Finished Haskell 1-3
    I also ran a formatter on it. Imma double check his HW3 help later to see if he does anything diff

--- haskell/Exercises.hs
+++ haskell/Exercises.hs
@@ -1,30 +1,55 @@
 module Exercises
-    ( change,
-      -- put the proper exports here
-    ) where
+  ( change,
+    firstThenApply,
+    powers,
+    meaningfulLineCount,
+  )
+where
 
-import qualified Data.Map as Map
-import Data.Text (pack, unpack, replace)
-import Data.List(isPrefixOf, find)
-import Data.Char(isSpace)
+import Control.Exception (handle)
+import Data.Char (isSpace)
+import Data.List (find, isPrefixOf)
+import Data.Map qualified as Map
+import Data.Text (pack, replace, unpack)
+import GHC.Data.ShortText (ShortText (contents))
+import GHC.IO.FD (openFile)
 
 change :: Integer -> Either String (Map.Map Integer Integer)
 change amount
-    | amount < 0 = Left "amount cannot be negative"
-    | otherwise = Right $ changeHelper [25, 10, 5, 1] amount Map.empty
-        where
-          changeHelper [] remaining counts = counts
-          changeHelper (d:ds) remaining counts =
-            changeHelper ds newRemaining newCounts
-              where
-                (count, newRemaining) = remaining `divMod` d
-                newCounts = Map.insert d count counts
+  | amount < 0 = Left "amount cannot be negative"
+  | otherwise = Right $ changeHelper [25, 10, 5, 1] amount Map.empty
+  where
+    changeHelper [] remaining counts = counts
+    changeHelper (d : ds) remaining counts =
+      changeHelper ds newRemaining newCounts
+      where
+        (count, newRemaining) = remaining `divMod` d
+        newCounts = Map.insert d count counts
 
--- Write your first then apply function here
+firstThenApply :: [a] -> (a -> Bool) -> (a -> b) -> Maybe b
+firstThenApply xs p f = f <$> find p xs
 
--- Write your infinite powers generator here
+powers :: (Integral a) => a -> [a]
+powers base = map (base ^) [0 ..]
 
--- Write your line count function here
+meaningfulLineCount :: FilePath -> IO Int
+meaningfulLineCount path = do
+  contents <- readFile path
+  let linesOfFile = lines contents
+      validLines = filter isValidLine linesOfFile
+  return (length validLines)
+
+isValidLine :: String -> Bool
+isValidLine line =
+  not (null trimmedLine)
+  && not (all isSpace trimmedLine)
+  && not (startsWithHash trimmedLine)
+  where
+    trimmedLine = dropWhile isSpace line
+
+startsWithHash :: String -> Bool
+startsWithHash [] = False
+startsWithHash (x : _) = x == '#'
 
 -- Write your shape data type here
 
