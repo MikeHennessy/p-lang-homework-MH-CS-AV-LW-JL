8990620be18d1339a4be94fc9b8412da5f0299f6
JackKingLeon <jackleon415@gmail.com>
2024-10-21 09:22:48 -0700

    Ocaml Code via Cameron
    not formatted or anything, ill download the language today and start that process later

--- ocaml/exercises.ml
+++ ocaml/exercises.ml
@@ -12,12 +12,85 @@ let change amount =
     in
     aux amount denominations
 
-(* Write your first then apply function here *)
+let first_then_apply (test_list: 'a list)
+                     (predicate: 'a -> bool)
+                     (function_to_apply: 'a -> 'b option)
+                     : 'b option = 
+  match List.find_opt predicate test_list with
+  | Some value -> (function_to_apply value)
+  | None -> None
 
-(* Write your powers generator here *)
+let powers_generator base =
+  let rec generate_from power () =
+    Seq.Cons (power, generate_from (power * base))
+  in
+  generate_from 1
 
-(* Write your line count function here *)
+let meaningful_line_count file_name = 
+  let meaningful_line line =
+    let trimmed = String.trim line in
+    String.length trimmed > 0 && not (String.starts_with trimmed ~prefix:"#")
+  in
+  let file = open_in file_name in
+  let finally () = close_in file in
+  let rec count_lines count =
+    try
+      let line = input_line file in
+      if meaningful_line line then
+        count_lines (count + 1)
+      else
+        count_lines count
+    with
+    | End_of_file -> count
+    in
+    Fun.protect ~finally (fun () -> count_lines 0)
 
-(* Write your shape type and associated functions here *)
+type shape =
+  | Sphere of float
+  | Box of float * float * float
 
-(* Write your binary search tree implementation here *)
+let volume shape = 
+  match shape with
+  | Sphere radius -> Float.pi *. (radius ** 3.) *. 4. /. 3.
+  | Box (length, width, height) -> length *. width *. height
+
+let surface_area shape = 
+  match shape with
+  | Sphere radius -> 4. *. Float.pi *. (radius ** 2.)
+  | Box (length, width, height) -> 2. *. (width *. length +. height *. length +. height *. width)
+
+type 'a binary_search_tree =
+  | Empty
+  | Node of 'a binary_search_tree * 'a * 'a binary_search_tree
+
+let rec insert value tree =
+  match tree with
+  | Empty -> Node (Empty, value, Empty)
+  | Node (left, old_value, right) ->
+    if value < old_value then
+      Node (insert value left, old_value, right)
+    else if value > old_value then
+      Node (left, old_value, insert value right)
+    else
+      tree
+
+let rec size tree =
+  match tree with
+  | Empty -> 0
+  | Node (left, _, right) -> 1 + size left + size right
+
+let rec contains value tree =
+  match tree with
+  | Empty -> false
+  | Node (left, old_value, right) ->
+    if value = old_value then
+      true
+    else if value < old_value then
+      contains value left
+    else
+      contains value right
+
+let rec inorder tree =
+  match tree with
+  | Empty -> []
+  | Node (left, old_value, right) -> inorder left @ [old_value] @ inorder right
